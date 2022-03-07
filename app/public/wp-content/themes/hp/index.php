0c060d856ac0b355e4c8c06e715bf95c2f6e3b1a
tomoya224 <kusunoki.t.224@gmail.com>
2022-03-07 23:49:46 +0900

    変更

--- app/public/wp-content/themes/hp/index.php
+++ app/public/wp-content/themes/hp/index.php
@@ -15,21 +15,39 @@
   <section id="reform">
     <div id="flexbox">
       <div>
-       <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
-       <h1>リノベーション・リフォーム</h1>
-       <p>暮らしに合わせた住まいの見直し</p>
+         <h1>リノベーション・リフォーム</h1>
+         <p>暮らしに合わせた住まいの見直し</p>
       </div>
       <div>
-       <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
-       <h2>新築・注文住宅</h2>
-       <p>光と風と木の住まい</p>
+         <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png"> 
       </div>
       <div>
-       <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
-       <h3>不動産・システム建築</h3>
-       <p>賃貸住宅・中古住宅・土地</p>
+         <h1>新築・注文住宅</h1>
+         <p>光と風と木の住まい</p>
       </div>
-    </div>
-    
+      <div>
+          <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
+      </div>
+      <div>
+         <h1>不動産・システム建築</h1>
+         <p>賃貸住宅・中古住宅・土地</p>
+      </div>
+      <div>
+         <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
+      </div>
+    </div>    
 </section>
+
+<div>
+  <img id="go-img" src="<?php echo get_template_directory_uri() ?>/images/sr.png">
+  <h1 class="go-h1">展示場にご来場希望のお客様へ</h1>
+  <p class="go-p">事前に来場予約をいただけましたら、ご指定の時間で見学いただけます。事前予約をぜひご活用ください。<br>
+                  各展示場では消毒などによる感染対策防止を徹底させていただいております。ご来場が重なった場合などには見学時間を調整させていただく場合がございます。</p>
+</div>
+
+<!-- イベント情報 -->
+<section id="event">
+ <h1 class="evet-h1">イベント情報</h1>
+ <div id="event-img">
+   <img src="<?php echo get_template_directory_uri() ?>/images/"
  
