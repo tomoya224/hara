1fd7f4d20bddf62093d260b732812c0f1605a4b0
tomoya224 <kusunoki.t.224@gmail.com>
2022-03-08 07:43:05 +0900

    変更

--- app/public/wp-content/themes/hp/index.php
+++ app/public/wp-content/themes/hp/index.php
@@ -14,26 +14,20 @@
 
   <section id="reform">
     <div id="flexbox">
-      <div>
+      <div class="div-img">
          <h1>リノベーション・リフォーム</h1>
          <p>暮らしに合わせた住まいの見直し</p>
+         <img src="<?php echo get_template_directory_uri() ?>/images/reform.png"> 
       </div>
-      <div>
-         <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png"> 
-      </div>
-      <div>
+      <div class="div-img">  
          <h1>新築・注文住宅</h1>
          <p>光と風と木の住まい</p>
+          <img src="<?php echo get_template_directory_uri() ?>/images/reform.png">
       </div>
-      <div>
-          <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
-      </div>
-      <div>
+      <div class="div-img">
          <h1>不動産・システム建築</h1>
          <p>賃貸住宅・中古住宅・土地</p>
-      </div>
-      <div>
-         <img id="div-img" src="<?php echo get_template_directory_uri() ?>/images/reform.png">
+         <img src="<?php echo get_template_directory_uri() ?>/images/reform.png">
       </div>
     </div>    
 </section>
