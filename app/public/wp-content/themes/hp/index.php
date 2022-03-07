87c04df1df510ed42ea1e13301f7e4c13e51f889
tomoya224 <kusunoki.t.224@gmail.com>
2022-03-08 07:56:40 +0900

    変更

--- app/public/wp-content/themes/hp/index.php
+++ app/public/wp-content/themes/hp/index.php
@@ -18,13 +18,13 @@
          <h1>リノベーション・リフォーム</h1>
          <p>暮らしに合わせた住まいの見直し</p>
          <img src="<?php echo get_template_directory_uri() ?>/images/reform.png"> 
-      </div>
-      <div class="div-img">  
+</div>
+<div class="div-img">
          <h1>新築・注文住宅</h1>
          <p>光と風と木の住まい</p>
           <img src="<?php echo get_template_directory_uri() ?>/images/reform.png">
-      </div>
-      <div class="div-img">
+</div>
+<div class="div-img">
          <h1>不動産・システム建築</h1>
          <p>賃貸住宅・中古住宅・土地</p>
          <img src="<?php echo get_template_directory_uri() ?>/images/reform.png">
