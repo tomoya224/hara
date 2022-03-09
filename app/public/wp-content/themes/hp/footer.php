560452629939a0f92c6ca3badb3c01516331075f
tomoya224 <kusunoki.t.224@gmail.com>
2022-03-10 00:02:13 +0900

    変更

--- app/public/wp-content/themes/hp/footer.php
+++ app/public/wp-content/themes/hp/footer.php
@@ -1,10 +1,19 @@
+
+
+</main>
 <footer>
     <div id="footer-link">
         <a href>新築・注文住宅</a>
+         <p href>原工務店の家づくり</p>
+         <p href>ZEHへの取り組み</P>
+         <p href>施工事例</p>
+         <p href>住まいの訪問記</p>
+         <p href>住宅展示場</p>
+         <p href>家づくりの流れ</p>
+         <p href>自然素材辞典</p>
         <a href>不動産</a>
         <a href>イベント情報</a>
     </div>
 </footer>
-</main>
 </body>
 </html>
