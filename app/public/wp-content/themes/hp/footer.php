20f0f06e2f50f45f8c08f9e413dfa43811e8aee5
tomoya224 <kusunoki.t.224@gmail.com>
2022-03-10 08:13:56 +0900

    変更

--- app/public/wp-content/themes/hp/footer.php
+++ app/public/wp-content/themes/hp/footer.php
@@ -11,8 +11,37 @@
          <p href>住宅展示場</p>
          <p href>家づくりの流れ</p>
          <p href>自然素材辞典</p>
+        <a href>リフォーム</a>
+         <p href>施工事例</p>
+         <p href>古民家再生</p>
+         <p href>山口ショールーム</p>
+         <p href>リフォームFAQ</p>
+         <p href>リフォームの流れ</p>
+</div>
+    <div id="footer-link">
         <a href>不動産</a>
+         <p href>不動産情報</p>
+         <p href>新築分譲住宅「アーバンキュービックキュービック松崎」
+         <p href>売り物件募集</p>  
+        <a href>企業情報</a>
+         <p href>会社概要</p>
+         <p href>沿革</p>
+         <p href>拠点</p>
+         <p href>スタッフ</p>
+         <p href>TVCM</p>
+</div>  
+    <div id="footer-link">
         <a href>イベント情報</a>
+         <p href>お問い合わせ・資料請求</p>
+         <p href>採用情報</p>
+         <p href>ブログ</p>
+         <p href>お知らせ</p>
+         <p href>個人情報保護方針</p>
+         <p href>サイトマップ</p>
+         <p href>もっくの森</p>
+         <p href>ハラ・ビレッジ</p>
+         <p href>ココチュール</p>
+         <p href>九州・山口匠の会</p>
     </div>
 </footer>
 </body>
